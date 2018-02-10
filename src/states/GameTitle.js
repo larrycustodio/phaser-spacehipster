d3c0f8ed88f7f54a3af216d3e29b342287868562
Chris Bremmer <c_bremmer@outlook.com>
2018-02-10 15:08:58 -0800

    Added GameTitle State

--- src/states/GameTitle.js
+++ src/states/GameTitle.js
@@ -1,13 +1,52 @@
 class GameTitle extends Phaser.State {
+  init() {
+    var score = score || 0;
+    this.highestScore = this.highestScore || 0;
 
-	create() {
+    this.highestScore = Math.max(score, this.highestScore);
+  }
+  create() {
+    //show the space tile, repeated
+    this.background = this.game.add.tileSprite(
+      0,
+      0,
+      this.game.width,
+      this.game.height,
+      "space"
+    );
 
-	}
+    //give it speed in x
+    this.background.autoScroll(-20, 0);
 
-	startGame() {
-		this.game.state.start("Main");
-	}
+    //start game text
+    var text = "Tap to begin";
+    var style = { font: "30px Arial", fill: "#fff", align: "center" };
+    var t = this.game.add.text(
+      this.game.width / 2,
+      this.game.height / 2,
+      text,
+      style
+    );
+    t.anchor.set(0.5);
 
+    //highest score
+    text = "Highest score: " + this.highestScore;
+    style = { font: "15px Arial", fill: "#fff", align: "center" };
+
+    var h = this.game.add.text(
+      this.game.width / 2,
+      this.game.height / 2 + 50,
+      text,
+      style
+    );
+    h.anchor.set(0.5);
+  }
+
+  update() {
+    if (this.game.input.activePointer.justPressed()) {
+      this.game.state.start("Main");
+    }
+  }
 }
 
 export default GameTitle;
